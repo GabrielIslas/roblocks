22e442b446eb2b8b11246cfe91a03d7b75d43e7f
DeZanatta15 <2109062@upy.edu.mx>
2022-04-01 07:38:34 -0600

    add min

---
+++ min_text.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include "min.h"
+#include <stdio.h>
+
+int main(){
+    int i;
+    int n; 
+    int Size;
+    int arr[10];
+
+    printf("Insert the Size of array: ");
+    for(i = 0; i < Size; i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Insert elemnts: ");
+    scanf("%d", &n);
+
+}
