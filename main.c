ed00345266af29cc617a21b1ca6a64914d0184f7
GabrielIslas <2109084@upy.edu.mx>
2022-04-04 07:59:59 -0500

    main almost complete

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
-#include "find.h";
-#include "odd.h";
+#include <stdio.h>
+#include "find.h"
+#include "odd.h"
+#include "max.h"
+
 
 int main(){
+    
+    int arrayTest[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    //find test
+    int findTest1 = find(arrayTest, 10, 4);
+    int findTest2 = find(arrayTest, 10, 11);
 
+    if(findTest1 == -1){
+        printf("find1 isn't in the array\n");
+    }
+    else{
+        printf("find1 is in position %d\n", findTest1);
+    }
+
+    if(findTest2 == -1){
+        printf("find2 isn't in the array\n");
+    }
+    else{
+        printf("find2 is in position %d\n", findTest2);
+    }
+
+    //odd test
+    int oddTest = odd(arrayTest, 10);
+    printf("There are %d odd numbers in the array\n", oddTest);
+
+    //max test
+    int maxTest = max(arrayTest, 10);
+    printf("Max number is in position %d\n", maxTest);
 }
--- odd.h
+++ odd.h
@@ -1,6 +1,6 @@
 int odd(int array[], int len) {
     int numberOdd = 0;
-    for(int i=0; i<5; i++) {
+    for(int i=0; i<len; i++) {
         if(array[i]%2!=0)
             numberOdd++;
     }
